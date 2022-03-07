e00c487b8657047e85475c1d66dd40328a65ffd2
Masud Afsar <masud@afsar.info>
2022-03-07 15:31:21 +0330

    Implement header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,10 @@
 import React from "react";
+import {Icon} from "@iconify/react"
+import IconMenu from '@iconify/icons-ic/twotone-menu';
+
 
 import styles from "./Header.module.scss";
+import {Container} from "../container/Container";
 
 interface Props {
 }
@@ -8,8 +12,12 @@ interface Props {
 export const Header: React.FC<Props> = ({}) => {
     return (
         <header className={styles.Header}>
-            <h1>Aparatgram</h1>
+            <Container className={styles.Container}>
+                <h1 className={styles.Title}>آپاراتگرام</h1>
+                <div className={styles.MenuButton}>
+                    <Icon icon={IconMenu} width={24}/>
+                </div>
+            </Container>
         </header>
-    )
-        ;
+    );
 }
